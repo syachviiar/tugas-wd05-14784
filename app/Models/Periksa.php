6afb1c055bddfe14d31f262acd7d147b72cd8879
syachviar <mawsyachviar4sch@gmail.com>
2025-03-23 16:07:52 +0700

    Menambahkan model dan migrasi Periksa, Obat, dan DetailPeriksa serta memperbaiki model User

---
+++ app/Models/Periksa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Periksa extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['id_pasien', 'id_dokter', 'tgl_periksa', 'catatan', 'biaya_periksa'];
+
+    public function pasien()
+    {
+        return $this->belongsTo(User::class, 'id_pasien');
+    }
+
+    public function dokter()
+    {
+        return $this->belongsTo(User::class, 'id_dokter');
+    }
+
+    public function detailPeriksa()
+    {
+        return $this->hasMany(DetailPeriksa::class, 'id_periksa');
+    }
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama');
+            $table->string('alamat', 255)->nullable();
+            $table->string('no_hp', 20);
+            $table->string('email', 50)->unique();
+            $table->string('role', 50);
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -47,3 +50,4 @@ return new class extends Migration
         Schema::dropIfExists('sessions');
     }
 };
+
